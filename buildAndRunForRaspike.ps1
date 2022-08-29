955753e25851aeb3452c885b76d4d367f8f24500
Yamato Sanekata <21jy0216@jec.ac.jp>
2022-08-29 20:54:46 +0900

    update buildAndRunForRaspike

--- buildAndRunForRaspike.ps1
+++ buildAndRunForRaspike.ps1
@@ -1,5 +1,6 @@
 # WSLのソースコードをRaspikeに転送して実行します。
 # RasPikeにSSH接続できる状態にしてから走らせてください
+# （我々はHackSPI使わないので、このスクリプトの説明は省きます）
 Param($projectName);
 scp -r localhost:~/workspace/$projectName pi@raspberrypi:~/work/RasPike/sdk/workspace
 ssh pi@raspberrypi "cd ~/work/RasPike/sdk/workspace; make img=$projectName; make start;"
