7f47ece23cd22a80d78a7781b8d4049c78793b6f
Yamato Sanekata <yamatomrp305@gmail.com>
2022-09-06 12:56:34 +0900

    add FacingAngle

--- command/FacingAngle.h
+++ command/FacingAngle.h
@@ -2,18 +2,19 @@
 #define FacingAngle_H
 
 #include "Command.h"
+#include "FinishConfirmable.h"
 #include "Walker.h"
 
 // 渡された角度を向くまで旋回し続けるコマンド。
 // スラローム進入後の段差での角度ずれを解決するためのもの。
 // 指定角度旋回したいのであれば、RotateRobotUseGyroCommandAndPredicateを使ってください
-class FacingAngle : public Command
+class FacingAngle : public Command, public FinishConfirmable
 {
 private:
     int pwm;
     int targetAngle;
     int angle;
-    bool finish;
+    bool finish = false;
 
     Walker *turnLeft;
     Walker *turnRight;
