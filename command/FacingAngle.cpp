fad06f1a893436f16b574ee7baac6cf7df631e4e
Yamato Sanekata <yamatomrp305@gmail.com>
2022-09-13 11:06:10 +0900

    add FacingAngle

---
+++ command/FacingAngle.cpp
@@ -0,0 +1,99 @@
+#include "FacingAngle.h"
+#include "RobotAPI.h"
+#include "Walker.h"
+#include "Stopper.h"
+#include "DebugUtil.h"
+
+FacingAngle::FacingAngle(FacingAngleMode mode, int pwm, int angle)
+{
+    this->mode = mode;
+    this->pwm = pwm;
+    this->angle = angle;
+
+    turnLeft = new Walker(-pwm, pwm);
+    turnRight = new Walker(pwm, -pwm);
+};
+
+FacingAngle::~FacingAngle(){};
+
+void FacingAngle::run(RobotAPI *robotAPI)
+{
+    int currentAngle = 0;
+    switch (mode)
+    {
+    case FA_Gyro:
+    {
+#ifndef SimulatorMode
+        currentAngle = robotAPI->getGyroSensor()->getAngle() * -1;
+#else
+        currentAngle = robotAPI->getGyroSensor()->getAngle();
+#endif
+        break;
+    }
+    case FA_WheelCount:
+    {
+        currentAngle = robotAPI->getMeasAngle()->getAngle();
+        break;
+    }
+    default:
+        break;
+    }
+
+    writeDebug("FacingAngle");
+    writeEndLineDebug();
+    writeDebug("angle: ");
+    writeDebug(currentAngle);
+    flushDebug(TRACE, robotAPI);
+
+    if (currentAngle == targetAngle)
+    {
+        finish = true;
+        Stopper *stopper = new Stopper();
+        stopper->run(robotAPI);
+        delete stopper;
+    }
+    else if (currentAngle < targetAngle)
+    {
+        turnRight->run(robotAPI);
+    }
+    else
+    {
+        turnLeft->run(robotAPI);
+    }
+}
+
+void FacingAngle::preparation(RobotAPI *robotAPI)
+{
+    int currentAngle = 0;
+    switch (mode)
+    {
+    case FA_Gyro:
+    {
+#ifndef SimulatorMode
+        currentAngle = robotAPI->getGyroSensor()->getAngle() * -1;
+#else
+        currentAngle = robotAPI->getGyroSensor()->getAngle();
+#endif
+        break;
+    }
+    case FA_WheelCount:
+    {
+        currentAngle = robotAPI->getMeasAngle()->getAngle();
+        break;
+    }
+    default:
+        break;
+    }
+    targetAngle = currentAngle + angle;
+    return;
+}
+
+FacingAngle *FacingAngle::generateReverseCommand()
+{
+    return new FacingAngle(mode, pwm, -angle);
+}
+
+bool FacingAngle::isFinished()
+{
+    return finish;
+}
---
+++ command/FacingAngle.h
@@ -0,0 +1,37 @@
+#ifndef FacingAngle_H
+#define FacingAngle_H
+
+#include "Command.h"
+#include "FinishConfirmable.h"
+#include "Walker.h"
+
+enum FacingAngleMode
+{
+    FA_Gyro,
+    FA_WheelCount,
+};
+
+// 渡された角度を向くまで旋回し続けるコマンド。
+// スラローム進入後の段差での角度ずれを解決するためのもの。
+// 指定角度旋回したいのであれば、RotateRobotUseGyroCommandAndPredicateを使ってください
+class FacingAngle: public Command, public FinishConfirmable
+{
+private:
+    FacingAngleMode mode;
+    int pwm;
+    int targetAngle;
+    int angle;
+    bool finish = false;
+
+    Walker *turnLeft;
+    Walker *turnRight;
+
+public:
+    FacingAngle(FacingAngleMode mode, int pwm, int angle);
+    ~FacingAngle();
+    virtual void run(RobotAPI *robotAPI) override;
+    virtual void preparation(RobotAPI *robotAPI) override;
+    virtual FacingAngle*generateReverseCommand() override;
+    virtual bool isFinished() override;
+};
+#endif
