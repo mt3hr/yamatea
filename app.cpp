d9e644195821f1af4e2733a67cde795155308206
Yamato Sanekata <yamatomrp305@gmail.com>
2022-09-15 22:18:10 +0900

    clean

--- app.cpp
+++ app.cpp
@@ -2054,8 +2054,6 @@ void initializeCommandExecutor(CommandExecutor *commandExecutor, RobotAPI *robot
   RawColorPredicate *blueEdgePredicate = new BlueEdgePredicate();
   commandExecutor->addCommand(lowPWMTracer, blueEdgePredicate, GET_VARIABLE_NAME(lowPWMTracer));
   commandExecutor->addCommand(stopper, new NumberOfTimesPredicate(1), GET_VARIABLE_NAME(stopper));
-
-  commandExecutor->reverseCommandAndPredicate(); // TODO けして
 }
 #endif
 
@@ -2717,7 +2715,6 @@ void initializeCommandExecutor(CommandExecutor *commandExecutor, RobotAPI *robot
   int rightPWM = -10;
   Walker *walker = new Walker(leftPWM, rightPWM);
   commandExecutor->addCommand(walker, new Predicate(), GET_VARIABLE_NAME(walker));
-  commandExecutor->reverseCommandAndPredicate(); // TODO けして
 }
 #endif
 
