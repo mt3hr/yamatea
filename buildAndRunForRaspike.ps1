5b1d36e1d989bb2dd6178115c749d9cfb167d561
Yamato Sanekata <yamatomrp305@gmail.com>
2022-10-05 20:08:33 +0900

    update buildAndRunForRaspike.ps1

--- buildAndRunForRaspike.ps1
+++ buildAndRunForRaspike.ps1
@@ -3,5 +3,9 @@
 # （我々はHackSPI使わないので、このスクリプトの説明は省きます）
 # 実方
 Param($projectName);
-scp -r localhost:~/workspace/$projectName pi@raspberrypi:~/work/RasPike/sdk/workspace
+$temp = "$HOME\temp"; # 直接scpすると上手く動かない時があったりしたので
+mkdir -Force $temp
+scp -r localhost:~/etrobo/workspace/$projectName $temp;
+ssh pi@raspberrypi rm -rf ~/work/RasPike/sdk/workspace/$projectName;
+scp -r $temp\$projectName pi@raspberrypi:~/work/RasPike/sdk/workspace;
 ssh pi@raspberrypi "cd ~/work/RasPike/sdk/workspace; make img=$projectName; make start;"
